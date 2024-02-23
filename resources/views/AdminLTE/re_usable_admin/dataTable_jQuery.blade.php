46aa1d63e1284f828f7be8edb54ab4acf9f5b111
needyamin <needyamin@gmail.com>
2024-02-23 22:19:31 +0600

    delete

--- resources/views/AdminLTE/re_usable_admin/dataTable_jQuery.blade.php
+++
@@ -1,6 +0,0 @@
-
-<!-- Include jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<!-- Include DataTables CSS and JS files -->
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
-    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
